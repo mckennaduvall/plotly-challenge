ee1876663ae6f396811c7e89e3a8603487fa778c
Mack D <mckenna.duvall@gmail.com>
2021-07-04 02:10:42 -0700

    Add bar data

--- static/js/app.js
+++ static/js/app.js
@@ -2,18 +2,44 @@
     d3.json("data/samples.json").then((data) => {
         console.log(data)
 
+// select the names of the data sets
         var subjectId = data.names
+        var sampleData = data.samples
         console.log(subjectId)
+        console.log(sampleData)
 
+// loop through each name and append names to drop down menu
         subjectId.forEach(function(id)  {
             var selectId = d3.select("#selDataset")
             var option = selectId.append("option");
             option.text(id)
-        
-            var initId = selectId.property("value")
-
-        
+            selectId.property("value")
         });
-    
-     });
 
+
+        var filterData = sampleData.filter(sample => sample.id === "940")
+        console.log(filterData[0])
+
+        filterData = filterData[0]
+
+        var barData = [{
+            x: filterData.sample_values.slice(0,10).reverse(), 
+            y: `${filterData.otu_ids.slice(0,10).reverse()}`,
+            text: filterData.otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h"
+        }];
+
+        var layout = {
+            title: "Belly Button"
+        }
+
+       
+        Plotly.newPlot("bar", barData, layout);
+
+
+
+        var dropDown = d3.select("#selDataset")
+        dropDown.on("change", optionChange)
+
+     });
